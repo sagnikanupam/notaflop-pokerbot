d297d7ca9a9ccdac1427aba3e02fa117ff555ffe
MuhenderRaj <rkmr02@gmail.com>
2022-01-08 16:36:53 -0500

    Renaming of bots

--- check_call/player.py
+++ check_call/player.py
@@ -37,19 +37,11 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        #### PARAMETERS ####
         my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
-
         game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
-
         round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
-
         my_cards = round_state.hands[active]  # your cards
-
         big_blind = bool(active)  # True if you are the big blind
-        
-        #### LOGIC ####
-
         pass
 
     def handle_round_over(self, game_state, terminal_state, active):
@@ -64,18 +56,11 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        #### PARAMETERS ####
         my_delta = terminal_state.deltas[active]  # your bankroll change from this round
-
         previous_state = terminal_state.previous_state  # RoundState before payoffs
-
         street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
-
         my_cards = previous_state.hands[active]  # your cards
         opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
-        
-        #### LOGIC ####
-
         pass
 
     def get_action(self, game_state, round_state, active):
@@ -91,31 +76,20 @@ class Player(Bot):
         Returns:
         Your action.
         '''
-        #### PARAMETERS ####
         legal_actions = round_state.legal_actions()  # the actions you are allowed to take
-
         street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
-        
         my_cards = round_state.hands[active]  # your cards
         board_cards = round_state.deck[:street]  # the board cards
-
         my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
         opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
-
         my_stack = round_state.stacks[active]  # the number of chips you have remaining
         opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
-
         continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
-
         my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
         opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
-
         if RaiseAction in legal_actions:
             min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
             min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
-            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
-        
-        #### LOGIC ####
 
         if CheckAction in legal_actions:  # check-call
             return CheckAction()
